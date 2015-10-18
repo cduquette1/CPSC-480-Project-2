8ccafe24091c3dc184847e88ef1945dffea755a7
cduquette1 <courtney.duquette.12@cnu.edu>
2015-10-17 22:02:20 -0400

    Shell for the Reversal JUnit class

---
+++ ReversalTest.java
@@ -0,0 +1,20 @@
+/**
+ * CPSC 480 Assignment 2: Reverse File Test Class
+ * @author Courtney Duquette
+ * @version 10/21/2014
+ */
+
+package project2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReversalTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
