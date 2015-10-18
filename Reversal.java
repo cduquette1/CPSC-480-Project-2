052f068ed243e9b91331ed9c4c2bc1366669891b
cduquette1 <courtney.duquette.12@cnu.edu>
2015-10-17 22:00:55 -0400

    Shell for the Reversal Class

---
+++ Reversal.java
@@ -0,0 +1,26 @@
+/**
+ * CPSC 480 Assignment 2: Reverse File
+ * @author Courtney Duquette
+ * @version 10/21/2014
+ */
+
+package project2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Reversal {
+
+    /**
+     * This method reverses lines of text received from a text file 
+     * and out prints it to a second output file.
+     * 
+     * @param input a text file with lines of text
+     * @param output the lines in the input file in reverse order
+     */
+    public static void reverseFile(File input,  File output) throws FileNotFoundException {
+        // TODO Complete method according to project assignment
+
+    }
+
+}
