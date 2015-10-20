50b77bb39aecf35d0bb16e00218beb0fbf357fa4
cduquette1 <courtney.duquette.12@cnu.edu>
2015-10-19 22:45:40 -0400

    Reversal Class Written

--- Reversal.java
+++ Reversal.java
@@ -4,10 +4,13 @@
  * @version 10/21/2014
  */
 
-package project2;
-
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Reversal {
 
@@ -19,8 +22,32 @@ public class Reversal {
      * @param output the lines in the input file in reverse order
      */
     public static void reverseFile(File input,  File output) throws FileNotFoundException {
-        // TODO Complete method according to project assignment
-
+        BufferedReader reader = new BufferedReader(new FileReader(input));
+        PrintWriter writer = new PrintWriter(output);
+        String text = null;
+        ArrayList<String> allWords = new ArrayList<String>();
+        
+        try {
+            while((text = reader.readLine()) != null) {
+                allWords.add(" ");
+                String[] words = text.split(" ");
+                for(int i = 0; i < words.length; i++) {
+                    allWords.add(words[i]);
+                }
+            }
+            
+            for(int i = (allWords.size() - 1); i >= 0 ; i--) {
+                if (!(allWords.get(i).equals(" "))) 
+                    writer.print(allWords.get(i) + " ");
+                else
+                    writer.println();
+            }
+            writer.close();
+            
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
-
 }
