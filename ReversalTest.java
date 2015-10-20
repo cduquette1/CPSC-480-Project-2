7cd13b5d7ef5ae899db67e63b1188fe7293f5039
cduquette1 <courtney.duquette.12@cnu.edu>
2015-10-19 23:35:33 -0400

    Methods were added to add text to a file and to covert a file to a string to compare against.

--- ReversalTest.java
+++ ReversalTest.java
@@ -7,19 +7,58 @@
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import org.junit.Test;
 
 public class ReversalTest {
 
     @Test(expected = FileNotFoundException.class)
-    public void test() throws FileNotFoundException {
+    public void testException() throws FileNotFoundException {
         File input = new File("temp.txt");
         File output = new File("temp.txt");
         
         Reversal.reverseFile(input, output);
+    }
+    
+    @Test
+    public void testOneLine() throws IOException {
+        File input = new File("Input.txt");
+        File output = new File("Output.txt");
+        
+        String expected = "amet sit dolor ipsum Lorem";
+        addTextToFile("Input.txt", "Lorem ipsum dolor sit amet");
+        addTextToFile("Output.txt", expected);
+        
+        Reversal.reverseFile(input, output);
+        String actual = covertFileToString("Output.txt");
+        
+        assertEquals("One line did not work", expected, actual);
+    }
+    
+    
+    
+    private void addTextToFile(String fileName, String text) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter(fileName);
+        writer.println(text);
+        writer.close();
+    }
+    
+    private String covertFileToString(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        String text = null;
+        String allWords = "";
+        
+        while((text = reader.readLine()) != null) {
+            allWords = allWords + text;
+        }
+        
+        return allWords;
     }
 
 }
